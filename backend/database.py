121769797c29839372ab7ea888a022b47ade5223
MistakeTZ <mistaketz@gmail.com>
2026-05-06 08:43:53 +0400

    postgres data

--- backend/database.py
+++ backend/database.py
@@ -4,8 +4,10 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./autoreviews.db")
 
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
