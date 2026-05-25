7bf856f1e9fbb74e5617b13c8c3986e4d5de79d1
MistakeTZ <mistaketz@gmail.com>
2026-05-25 10:37:33 +0400

    migrate to nginx

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,8 @@
 import type { NextConfig } from "next";
 
-const backendOrigin = process.env.BACKEND_INTERNAL_URL || 'http://backend:8000';
-
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.0.60'],
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/admin',
-        destination: '/admin/',
-        permanent: false,
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/admin/',
-        destination: `${backendOrigin}/admin/`,
-      },
-      {
-        source: '/admin/:path*',
-        destination: `${backendOrigin}/admin/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
